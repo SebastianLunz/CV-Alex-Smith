a0c691a48cc43c4eb3f51ef46af6ac41ee98af30
SebastianLunz <lunz.sebastian@gmail.com>
2021-03-17 16:36:35 +0100

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+// console.log("Czy działa js?");
+let navigation = document.getElementById("navigation");
+// console.dir(navigation);
+
+window.addEventListener("scroll", function () {
+    let scrollYposition = window.pageYOffset;
+    // console.log(scrollYposition);
+
+    if (scrollYposition >= 100) {
+        navigation.classList.add("small");
+    }
+    else {
+        navigation.classList.remove("small");
+    }
+})
+
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    navigation.classList.toggle("menu-open");
+})
